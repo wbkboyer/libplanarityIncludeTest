c800a42ebeb53e29bdd9bed3abb6fad5afb0dc6d
Wanda Boyer <wbkboyer@gmail.com>
2024-12-06 18:21:57 -0800

    Initial project setup; currently doesn't #include any planarity-specific headers

---
+++ test.c
@@ -0,0 +1,8 @@
+#include "test.h"
+
+int main(void)
+{
+    printf("Hello, world!\n");
+
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
