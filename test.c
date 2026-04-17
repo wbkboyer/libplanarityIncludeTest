fea1a3180bcbb1f6e42ecb8ecd215f45f72dfaf4
Wanda Boyer <wbkboyer@gmail.com>
2026-04-17 16:27:10 -0700

    Updates to verify full workflow for leveraging libplanarity

--- test.c
+++ test.c
@@ -1,20 +1,121 @@
 #include "test.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int retVal = 0;
+    int order = 0, maxNumEdges = 0, embedFlags = 0, embedResult = 0, embedIntegrityCheckResult = 0;
+    graphP graphToEmbed = NULL, graphToCheckEmbeddingIntegrity = NULL;
 
-    printf("Hello, world!\n");
-
-    graphP theGraph = gp_New();
-    retVal = gp_InitGraph(theGraph, 8);
-    if (retVal != OK)
+    if (argc != 2)
     {
-        printf("Oh boy, couldn't initialie graph!\n");
-        return retVal;
+        printf("No graph input filename provided.\n");
+        return -1;
     }
 
-    printf("Successfully initialized graphP!\n");
+    graphToEmbed = gp_New();
+    if (graphToEmbed == NULL)
+    {
+        printf("Unable to allocate graph to embed.\n");
+        return -1;
+    }
 
-    return retVal;
+    graphToCheckEmbeddingIntegrity = gp_New();
+    if (graphToCheckEmbeddingIntegrity == NULL)
+    {
+        printf("Unable to allocate graph to check embed result integrity.\n");
+
+        gp_Free(&graphToEmbed);
+
+        return -1;
+    }
+
+    if (gp_Read(graphToEmbed, argv[1]) != OK)
+    {
+        printf("Unable to read %s into graphP\n", argv[1]);
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    order = gp_GetN(graphToEmbed);
+
+    if (gp_InitGraph(graphToCheckEmbeddingIntegrity, order) != OK)
+    {
+        printf("Unable to initialize graph to check embed result integrity.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    maxNumEdges = (order * (order - 1)) / 2;
+    if (gp_EnsureEdgeCapacity(graphToEmbed, maxNumEdges) != OK)
+    {
+        printf("Failed to Ensure Edge Capacity of graph.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    if (gp_CopyGraph(graphToCheckEmbeddingIntegrity, graphToEmbed) != OK)
+    {
+        printf("Failed to make a copy of the graph for embedding integrity check.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    if (gp_ExtendWith_K33Search(graphToEmbed) != OK)
+    {
+        printf("Failed to extend graph to embed with structures necessary for K_{3,3} homeomorph search.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    embedFlags = EMBEDFLAGS_SEARCHFORK33;
+    embedResult = gp_Embed(graphToEmbed, embedFlags);
+    if (embedResult != OK && embedResult != NONEMBEDDABLE)
+    {
+        printf("Failed to run K_{3,3} homeomorph search.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    embedIntegrityCheckResult = gp_TestEmbedResultIntegrity(graphToEmbed, graphToCheckEmbeddingIntegrity, embedResult);
+    if (embedIntegrityCheckResult != OK && embedIntegrityCheckResult != NONEMBEDDABLE)
+    {
+        printf("Embed result integrity check for K_{3,3} homeomorph search failed.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    if (gp_Write(graphToEmbed, "test_output.txt", WRITE_ADJLIST) != OK)
+    {
+        printf("Failed to write graph.\n");
+
+        gp_Free(&graphToEmbed);
+        gp_Free(&graphToCheckEmbeddingIntegrity);
+
+        return -1;
+    }
+
+    gp_Free(&graphToEmbed);
+    gp_Free(&graphToCheckEmbeddingIntegrity);
+
+    return 0;
 }
